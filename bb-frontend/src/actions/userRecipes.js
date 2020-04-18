07d60e8bdf2bffce7bb1675775b9f27571155855
amfosh <50755533+amfosh@users.noreply.github.com>
2020-04-18 11:05:25 -0400

    added userRecipes to store

---
+++ bb-frontend/src/actions/userRecipes.js
@@ -0,0 +1,6 @@
+export const setUserRecipes = recipes => {
+    return {
+        type: "SET_USER_RECIPES",
+        recipes
+    }
+}
--- bb-frontend/src/store.js
+++ bb-frontend/src/store.js
@@ -1,13 +1,15 @@
 import usersReducer from './reducers/users'
 import currentUser from './reducers/currentUser'
 import loginForm from './reducers/loginForm'
+import userRecipes from './reducers/userRecipes'
 import { createStore, applyMiddleware, compose, combineReducers } from 'redux';
 import thunk from 'redux-thunk'
 
 const reducer = combineReducers({
   users: usersReducer,
   currentUser,
-  loginForm
+  loginForm,
+  userRecipes
 })
 const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
