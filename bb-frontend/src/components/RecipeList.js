31385f79fa9a521ffbbba1292d38f7c3501b5fa1
amfosh <50755533+amfosh@users.noreply.github.com>
2020-04-21 08:58:31 -0400

    styling recipe list

--- bb-frontend/src/components/RecipeList.js
+++ bb-frontend/src/components/RecipeList.js
@@ -5,8 +5,12 @@ import { connect } from 'react-redux'
 import { Link } from 'react-router-dom'
 
 const RecipeList = props => {
+    
     const recipeCards = props.recipes.length > 0 ?
-    props.recipes.map(r => (<p key={r.id}><Link to={`/recipes/${r.id}`}>{r.attributes.label}</Link><br/></p>)) : null
+    props.recipes.map(r => (<div key={r.id}>
+        <Link to={`/recipes/${r.id}`}>
+            <p>{r.attributes.label}</p></Link>
+            <p><img src={r.attributes.image} alt="bread"/></p><br/></div>)) : null
     return recipeCards 
 }
 
